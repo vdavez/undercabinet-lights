6e87b6090a90f5cb6b98df71549b82504096f2c2
V. David Zvenyach <dave@zvenyach.com>
2022-10-10 18:13:02 -0500

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Under Cabinet Lights
+
+Controlling LEDs using a potentiometer
---
+++ main.py
@@ -0,0 +1,21 @@
+import machine
+import neopixel
+import time
+import math
+
+# Get potentiometer reading
+adc = machine.ADC(26)
+# Initialize LEDs
+np = neopixel.NeoPixel(machine.Pin(0, machine.Pin.OUT), 30)
+
+while True:
+
+    # Get the potentiometer reading, convert to RGB 0 to 255 
+    b = int(2.55 * int(adc.read_u16() / 655.35))
+    print(adc.read_u16(), int(adc.read_u16() * 100 / 65535), b)
+    # Write to the Neopixels
+    np.fill((b,b,b))
+    np.write()
+
+    # Wait 0.1 seconds and loop
+    time.sleep(0.1)
